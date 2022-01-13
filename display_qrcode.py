ad627ab54803fd6d3f75fb320250d27160ace968
David Spiegelman <david94609@gmail.com>
2022-01-12 20:01:43 -0800

    Add files via upload

---
+++ display_qrcode.py
@@ -0,0 +1,24 @@
+import serial
+import pyqrcode
+
+ERROR = 'M'
+QUIET_ZONE = 1
+PORT = '/dev/serial0'
+BAUD = 115200
+CLEAR_LINES = 40
+
+
+done = False
+
+with serial.Serial(PORT, BAUD, timeout=1) as ser:
+  while not done:
+    line = ser.readline()
+    if line != b'':
+      line = line[:-1]
+      if line == b'quit':
+        done = True
+      elif line == b'clear':
+        _ = [print() for x in range(CLEAR_LINES)]
+      else:
+        qr = pyqrcode.create(line, error=ERROR)
+        print(qr.terminal(quiet_zone=QUIET_ZONE))
