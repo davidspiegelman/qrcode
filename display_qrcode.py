3ef9ed91b47dc20f9b46119da5e4224f25cccbfd
David Spiegelman <david94609@gmail.com>
2022-01-13 11:16:55 -0800

    clear lines at start

--- display_qrcode.py
+++ display_qrcode.py
@@ -2,12 +2,15 @@ import serial
 import pyqrcode
 
 ERROR = 'M'
-QUIET_ZONE = 1
+QUIET_ZONE = 4
 PORT = '/dev/serial0'
 BAUD = 115200
-CLEAR_LINES = 40
+CLEAR_LINES = 50
 
+def clear_lines():
+  print('\n' * CLEAR_LINES)
 
+clear_lines()
 done = False
 
 with serial.Serial(PORT, BAUD, timeout=1) as ser:
@@ -18,7 +21,7 @@ with serial.Serial(PORT, BAUD, timeout=1) as ser:
       if line == b'quit':
         done = True
       elif line == b'clear':
-        print('\n' * CLEAR_LINES)
+        clear_lines()
       else:
         qr = pyqrcode.create(line, error=ERROR)
         print(qr.terminal(quiet_zone=QUIET_ZONE))
