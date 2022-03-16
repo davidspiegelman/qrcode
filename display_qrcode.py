1812719c06cd8828c0b68ba780d9ce94184004c9
David Spiegelman <david94609@gmail.com>
2022-03-15 18:37:15 -0700

    Handle incomplete lines

--- display_qrcode.py
+++ display_qrcode.py
@@ -2,10 +2,11 @@ import serial
 import pyqrcode
 
 ERROR = 'M'
-QUIET_ZONE = 4
+QUIET_ZONE = 1
 PORT = '/dev/serial0'
 BAUD = 115200
 CLEAR_LINES = 50
+NEW_LINE = '\n'
 
 def clear_lines():
   print('\n' * CLEAR_LINES)
@@ -14,14 +15,16 @@ clear_lines()
 done = False
 
 with serial.Serial(PORT, BAUD, timeout=0.9) as ser:
+  line = ''
   while not done:
-    line = ser.readline()
-    if line != b'':
+    line = line + ser.readline().decode(errors='ignore')
+    if line != '' and line.endswith(NEW_LINE):
       line = line[:-1]
-      if line == b'quit':
+      if line == 'quit':
         done = True
-      elif line == b'clear':
+      elif line == 'clear':
         clear_lines()
       else:
         qr = pyqrcode.create(line, error=ERROR)
         print(qr.terminal(quiet_zone=QUIET_ZONE))
+      line = ''
